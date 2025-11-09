df854e1eb380a65cf6ce87eec89800941bb49652
TDianaAle <dianaalexandra.tichy@studio.unibo.it>
2025-11-09 16:49:18 +0100

    add tailwind config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router'; // Importa il router
-
+// IMPORT TAILWIND
 import './assets/main.css';
 const app = createApp(App);
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+import { defineConfig } from 'tailwindcss'
+
+export default defineConfig({
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+})
